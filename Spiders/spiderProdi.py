81f489d093c41619dd6a24562cdb48d48dc560e3
Muhammad Kharisma Azhari <39398937+muazhari@users.noreply.github.com>
2019-07-10 21:30:17 +0700

    Update

--- Spiders/spiderProdi.py
+++ Spiders/spiderProdi.py
@@ -13,12 +13,12 @@ def replaceStrByIndex(string, i, newLetter):
 
 class spiderProdi(scrapy.Spider):
     df = pd.read_csv("dataPTN.csv")
-    ptnData = df['kode_ptn'].drop_duplicates().astype(str)
+    ptnData = df['kode_ptn'].astype(str)
 
     name = 'spiderProdi'
     allowed_domains = ['sbmptn.ltmpt.ac.id']
 
-    start_urls = ['https://sbmptn.ltmpt.ac.id/index.php?mid=14&ptn={ptn}&jenis=0'.format(
+    start_urls = ['https://sbmptn.ltmpt.ac.id/index.php?mid=14&ptn={ptn}&jenis=`'.format(
         ptn=ptn) for ptn in ptnData]
 
     base_url = 'https://sbmptn.ltmpt.ac.id/'
@@ -38,7 +38,8 @@ class spiderProdi(scrapy.Spider):
     def parse(self, response):
         MAIN_XPATH = '/html/body/div[2]/div/div[2]/div/div[1]/div/div[2]/div[2]'
 
-        TABLE_XPATH = MAIN_XPATH + '/div/div/table[1]/tbody'
+        TABLE_XPATH = MAIN_XPATH + \
+            '/div/div[position()>0 and position()<=last()]/table[1]/tbody'
         TABLE_SELECTOR = response.xpath(TABLE_XPATH)
 
         RECORD_XPATH = './/tr[position()>0 and position()<=last()]'
@@ -47,33 +48,23 @@ class spiderProdi(scrapy.Spider):
         for rset in RECORD_SELECTOR:
             DATA = {}
 
+            sebaran_link = rset.xpath('.//td[6]/a/@href').extract_first()
+
+            sebaran_url = self.base_url + sebaran_link
+
             DATA['kode_prodi'] = rset.xpath(
                 './/td[1]/text()').extract_first(),
             DATA['nama_prodi'] = rset.xpath(
                 './/td[2]/text()').extract_first(),
+            DATA['rumpun'] = sebaran_link[-1],
             DATA['daya_tampung_2019'] = rset.xpath(
                 './/td[3]/text()').extract_first(),
             # DATA['peminat_2018'] = rset.xpath('.//td[4]/text()').extract_first(),
-
-            sebaran_link = rset.xpath('.//td[6]/a/@href').extract_first()
-
-            sebaran_url = self.base_url + sebaran_link
 
             yield scrapy.Request(
                 response.urljoin(sebaran_url),
                 callback=self.parse_sebaran,
                 meta={'data': DATA}
-            )
-
-        RUMPUN_XPATH = MAIN_XPATH + '/ul/li[last()]/div/a/@href'
-        RUMPUN_SELECTOR = response.xpath(RUMPUN_XPATH).extract_first()
-        if RUMPUN_SELECTOR and response.request.url[-1] != RUMPUN_SELECTOR[-1]:
-            RUMPUN_url = replaceStrByIndex(
-                response.request.url, -1, RUMPUN[-1])
-
-            yield scrapy.Request(
-                response.urljoin(RUMPUN_url),
-                callback=self.parse
             )
 
     def parse_sebaran(self, response):
